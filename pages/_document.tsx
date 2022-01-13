20e96d7eec02d5c065b98491d8b88cf1c890f590
Andres Matte <matte.andres@gmail.com>
2022-01-13 14:42:56 -0300

    chore(): add gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { GTMProvider } from "@elgorditosalsero/react-gtm-hook";
-import Document, { DocumentContext, Head, Main, NextScript, DocumentProps } from "next/document";
+import Document, { DocumentContext, Head, Html, Main, NextScript, DocumentProps } from "next/document";
 
 type Props = Record<string, unknown> & DocumentProps;
 
@@ -12,21 +12,23 @@ class MyDocument extends Document<Props> {
     const gtmParams = { id: "GTM-PJQRG9F" };
 
     return (
-      <GTMProvider state={gtmParams}>
-        <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-          <meta name="msapplication-TileColor" content="#ff0000" />
-          <meta name="theme-color" content="#ffffff" />
-        </Head>
-        <body className="bg-gray-100 dark:bg-black">
-          <Main />
-          <NextScript />
-        </body>
-      </GTMProvider>
+      <Html>
+        <GTMProvider state={gtmParams}>
+          <Head>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+            <meta name="msapplication-TileColor" content="#ff0000" />
+            <meta name="theme-color" content="#ffffff" />
+          </Head>
+          <body className="bg-gray-100 dark:bg-black">
+            <Main />
+            <NextScript />
+          </body>
+        </GTMProvider>
+      </Html>
     );
   }
 }
