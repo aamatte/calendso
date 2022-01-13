5db49fcce38dc27f2cd867545031abb941623a20
Andres Matte <matte.andres@gmail.com>
2022-01-13 14:53:26 -0300

    chore(): add gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,3 @@
-import { GTMProvider } from "@elgorditosalsero/react-gtm-hook";
 import Document, { DocumentContext, Head, Html, Main, NextScript, DocumentProps } from "next/document";
 
 type Props = Record<string, unknown> & DocumentProps;
@@ -9,25 +8,38 @@ class MyDocument extends Document<Props> {
     return { ...initialProps };
   }
   render() {
-    const gtmParams = { id: "GTM-PJQRG9F" };
-
     return (
       <Html>
-        <GTMProvider state={gtmParams}>
-          <Head>
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest" />
-            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-            <meta name="msapplication-TileColor" content="#ff0000" />
-            <meta name="theme-color" content="#ffffff" />
-          </Head>
-          <body className="bg-gray-100 dark:bg-black">
-            <Main />
-            <NextScript />
-          </body>
-        </GTMProvider>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `<!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PJQRG9F');</script>
+            <!-- End Google Tag Manager -->`,
+            }}></script>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+          <meta name="msapplication-TileColor" content="#ff0000" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <body className="bg-gray-100 dark:bg-black">
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `<!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJQRG9F"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->`,
+            }}></script>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     );
   }
