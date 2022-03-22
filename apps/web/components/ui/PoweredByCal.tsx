0ab57ab0d852a973bf45f22b775d84d1a12277d7
Andres Matte <matte.andres@gmail.com>
2022-03-22 20:57:55 -0300

    feat(pv): change to powered by pv

--- apps/web/components/ui/PoweredByCal.tsx
+++ apps/web/components/ui/PoweredByCal.tsx
@@ -5,19 +5,19 @@ import { useLocale } from "@lib/hooks/useLocale";
 const PoweredByCal = () => {
   const { t } = useLocale();
   return (
-    <div className="p-1 text-center text-xs sm:text-right">
-      <Link href={`https://cal.com?utm_source=embed&utm_medium=powered-by-button`}>
+    <div className="p-1 text-xs text-center sm:text-right">
+      <Link href={`https://ventures.platan.us?utm_source=embed&utm_medium=powered-by-button-pv-cal`}>
         <a target="_blank" className="text-gray-500 opacity-50 hover:opacity-100 dark:text-white">
           {t("powered_by")}{" "}
           <img
             className="relative -mt-px inline h-[10px] w-auto dark:hidden"
-            src="https://cal.com/logo.svg"
-            alt="Cal.com Logo"
+            src="/pv-logo.svg"
+            alt="PV Logo"
           />
           <img
-            className="relativ -mt-px hidden h-[10px] w-auto dark:inline"
-            src="https://cal.com/logo-white.svg"
-            alt="Cal.com Logo"
+            className="relative -mt-px hidden h-[10px] w-auto dark:inline"
+            src="/pv-logo.svg"
+            alt="PV Logo"
           />
         </a>
       </Link>
